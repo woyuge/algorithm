1acf12908ebbf7b161731ef652e1b34e80da277b
woyuge <782975963@qq.com>
2021-04-29 16:54:59 +0800

    存在重复元素,用求字典的方式去判断 判断字符串s是不是字符串t的字母异位词

--- src/Array/checkRepeat.go
+++ src/Array/checkRepeat.go
@@ -1,1 +1,39 @@
 package main
+
+//存在重复元素,用求字典的方式去判断
+func main()  {
+
+	a :=[]int{1,2,3,4,5,6,6,6,6,7,7}
+	//b:= containsDuplicate(a)
+
+	c:=containsDuplicateHaset(a)
+	println(c)
+}
+
+//哈希表算法
+func containsDuplicateHaset(nums []int) bool {
+	m0 := map[int]int{}
+	for _,v := range nums{
+		if _,ok:= m0[v];ok {
+			return true
+		}
+		m0[v]++
+	}
+	return false
+}
+
+//手写map，算法不算太好
+func containsDuplicate(nums []int) bool {
+
+	m0 := map[int]int{}
+	for _,v := range nums{
+		m0[v]+=1
+	}
+
+	for _,v := range m0{
+		if v>1 {
+			return true
+		}
+	}
+	return false
+}
--- src/Str/isAnagram.go
+++ src/Str/isAnagram.go
@@ -1,1 +1,44 @@
 package main
+
+import "fmt"
+
+//判断字符串s是不是字符串t的字母异位词
+func main()  {
+
+	s := "ac"
+	t := "bb"
+
+	b := isAnagram(s, t)
+	fmt.Println(b)
+}
+func isAnagram(s,t string) bool{
+
+	if len(s)!= len(t) {
+		return false
+	}
+	var c1,c2 [26]int
+	for _, ch := range s {
+		c1[ch-'a']++
+	}
+	for _, ch := range t {
+		c2[ch-'a']++
+	}
+	//判断数组是否相等
+	return c1 == c2
+}
+func isAnagram2(s,t string) bool  {
+	if len(s) != len(t) {
+		return false
+	}
+	cnt := map[rune]int{}
+	for _, ch := range s {
+		cnt[ch]++
+	}
+	for _, ch := range t {
+		cnt[ch]--
+		if cnt[ch] < 0 {
+			return false
+		}
+	}
+	return true
+}
